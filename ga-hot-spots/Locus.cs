1dae4492e7c7f97de7aa4768dadaf119b7716bbb
Mike Kelly <michaelkelly322@gmail.com>
2014-02-14 17:39:55 -0500

    Initial commit, created project, basic Locus

---
+++ ga-hot-spots/Locus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ga_hot_spots
+{
+    class Locus
+    {
+        protected byte allele;
+        protected bool hotspot;
+
+        public byte Allele
+        {
+            get { return allele; }
+            set { allele = value; }
+        }
+
+        public bool Hotspot
+        {
+            get { return hotspot; }
+            set { hotspot = value; }
+        }
+
+        public Locus ()
+        {
+            allele = 0;
+            hotspot = false;
+        }
+
+        public Locus(byte in_allele, bool in_hotspot = false)
+        {
+            allele = in_allele;
+            hotspot = in_hotspot;
+        }
+    }
+}
---
+++ ga-hot-spots/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ga_hot_spots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
